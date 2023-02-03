6d223cec135275df358ad113454b395475efd4ce
LudoBernard <ludo.bernard.pro@gmail.com>
2023-02-03 21:52:00 +0100

    Seed spawning done

--- Assets/_Project/Scripts/SeedSpawning.cs
+++ Assets/_Project/Scripts/SeedSpawning.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SeedSpawning : MonoBehaviour
 {
-    [SerializeField] private float _spawnRate = 0.0f;
+    [SerializeField] private float _spawnRate = 3.0f;
     [SerializeField] private GameObject _seed;
-    
+    [SerializeField] private float _maxLength = 11.0f;
+    [SerializeField] private float _minLength = -11.0f;
+    private Vector2 _spawnPos;
+    private float _timer = 0.0f;
     void Update()
     {
-        
+        _spawnPos = gameObject.transform.position;
+        var rot = gameObject.transform.rotation;
+        _spawnPos.x = Random.Range(_minLength, _maxLength);
+        _timer += Time.deltaTime;
+        if (_timer >= _spawnRate)
+        {
+            Instantiate(_seed, _spawnPos, rot);
+            _timer = 0;
+        }
     }
 }
