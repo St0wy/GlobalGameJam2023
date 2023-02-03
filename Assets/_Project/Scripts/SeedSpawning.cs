5b47d71f963de979f9f23bc25e4eafc03e3efa93
LudoBernard <ludo.bernard.pro@gmail.com>
2023-02-03 21:12:26 +0100

    Prefabs and sprites

---
+++ Assets/_Project/Scripts/SeedSpawning.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedSpawning : MonoBehaviour
+{
+    [SerializeField] private float _spawnRate = 0.0f;
+    [SerializeField] private GameObject _seed;
+    
+    void Update()
+    {
+        
+    }
+}
