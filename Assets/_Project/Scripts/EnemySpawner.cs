681833c10022d4e21c2df8fea62967ff37bba1bb
André <a.silva14@hotmail.com>
2023-02-03 21:40:29 +0100

    Spawning done

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -7,15 +7,17 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] List<GameObject> _enemies = new List<GameObject>();
     [SerializeField] float _spawnRate;
-    [SerializeField] Vector2 _spawnPos;
+    [SerializeField] Vector2 _spawnPos ;
     
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+       
     }
 
     IEnumerator SpawnEnemy()
     {
+        _spawnPos = gameObject.transform.position;
         _spawnRate = Random.Range(0.2f, 2.4f);
         Instantiate(_enemies[0], _spawnPos, Quaternion.identity);
         yield return new WaitForSeconds(_spawnRate);
