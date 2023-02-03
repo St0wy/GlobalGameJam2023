0f0d3772c654519ecfb153c234cf807bdd4d125e
André <a.silva14@hotmail.com>
2023-02-03 21:30:54 +0100

    EnemySpawner random spawnRate

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -8,7 +8,6 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] List<GameObject> _enemies = new List<GameObject>();
     [SerializeField] float _spawnRate;
     [SerializeField] Vector2 _spawnPos;
-    private float _x, _y;
     
     void Start()
     {
@@ -17,6 +16,7 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemy()
     {
+        _spawnRate = Random.Range(0.2f, 2.4f);
         Instantiate(_enemies[0], _spawnPos, Quaternion.identity);
         yield return new WaitForSeconds(_spawnRate);
         StartCoroutine(SpawnEnemy());
