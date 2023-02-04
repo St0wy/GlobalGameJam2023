06d53c6c36cd63b24fd0bcd82813b2a627fe087b
Stowy <stowy@protonmail.ch>
2023-02-04 11:13:22 +0100

    added game over screen

---
+++ Assets/_Project/Scripts/UI/GameOver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MyBox;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GlobalGameJam.UI
+{
+	public class GameOver : MonoBehaviour
+	{
+		[MustBeAssigned]
+		[SerializeField]
+		private Health _playerHealth;
+
+		[MustBeAssigned]
+		[SerializeField]
+		private GameObject _gameOverUI;
+
+		[MustBeAssigned]
+		[SerializeField]
+		private Button _mainMenuButton;
+
+		[SerializeField]
+		private SceneReference _mainMenuScene;
+
+		private void OnEnable()
+		{
+			_playerHealth.OnDeath += OnDeath;
+		}
+
+		private void OnDisable()
+		{
+			_playerHealth.OnDeath -= OnDeath;
+		}
+
+		private void OnDeath()
+		{
+			_gameOverUI.SetActive(true);
+			_mainMenuButton.Select();
+			Time.timeScale = 0f;
+		}
+
+		public void GoToMainMenu()
+		{
+			Time.timeScale = 1f;
+			_mainMenuScene.LoadScene();
+		}
+	}
+}
