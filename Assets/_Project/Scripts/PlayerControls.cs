7a1e13de29cfb13161db66a6b63261ed3cee77c9
Stowy <stowy@protonmail.ch>
2023-02-04 23:02:29 +0100

    fixed dig bug

--- Assets/_Project/Scripts/PlayerControls.cs
+++ Assets/_Project/Scripts/PlayerControls.cs
@@ -1,190 +1,199 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace GlobalGameJam
 {
-    public class PlayerControls : MonoBehaviour
-    {
-        [SerializeField] private float _moveSpeed = 1f;
+	public class PlayerControls : MonoBehaviour
+	{
+		[SerializeField] private float _moveSpeed = 1f;
 
-        [SerializeField] private GameObject _projectilePrefab;
+		[SerializeField] private GameObject _projectilePrefab;
 
-        [SerializeField] private float _timeToShotAgainInSeconds = 0.3f;
+		[SerializeField] private float _timeToShotAgainInSeconds = 0.3f;
 
-        [SerializeField] private float _knockbackDrag = 0.05f;
+		[SerializeField] private float _knockbackDrag = 0.05f;
 
-        [SerializeField] private Transform _maxLeft;
+		[SerializeField] private Transform _maxLeft;
 
-        [SerializeField] private Transform _maxRight;
+		[SerializeField] private Transform _maxRight;
 
-        [SerializeField] private Slider _slider;
+		[SerializeField] private Slider _slider;
 
-        // [SerializeField]
-        // private int _maxAmmo;
+		// [SerializeField]
+		// private int _maxAmmo;
 
-        [SerializeField] private int _ammo;
+		[SerializeField] private int _ammo;
 
-        [SerializeField] private GameObject _pickupAmmoObject;
+		[SerializeField] private GameObject _pickupAmmoObject;
 
-        private Rigidbody2D _rigidbody;
-        private Vector2 _moveDirection;
-        private Vector2 _shootDirection;
-        private Camera _mainCam;
-        private float _shootTimer;
-        private bool _mousePressed;
-        private bool _isDigging;
+		private Rigidbody2D _rigidbody;
+		private Vector2 _moveDirection;
+		private Vector2 _shootDirection;
+		private Camera _mainCam;
+		private float _shootTimer;
+		private bool _mousePressed;
+		private bool _isDigging;
 
-        [field: SerializeField] public float KnockbackVelocityX { get; set; }
+		[field: SerializeField] public float KnockbackVelocityX { get; set; }
 
-        public Vector2 Movement { get; private set; }
+		public Vector2 Movement { get; private set; }
 
-        [SerializeField] private AudioPlayer _audioPlayer;
+		[SerializeField] private AudioPlayer _audioPlayer;
 
-        public Vector2 Input
-        {
-            get => _moveDirection;
-            set
-            {
-                _moveDirection = value;
-                if (_moveDirection.sqrMagnitude > _moveDirection.normalized.sqrMagnitude)
-                    _moveDirection.Normalize();
-            }
-        }
+		public Vector2 Input
+		{
+			get => _moveDirection;
+			set
+			{
+				_moveDirection = value;
+				if (_moveDirection.sqrMagnitude > _moveDirection.normalized.sqrMagnitude)
+					_moveDirection.Normalize();
+			}
+		}
 
-        public Vector2 LookDir
-        {
-            get => _shootDirection;
-            set => _shootDirection = value;
-        }
+		public Vector2 LookDir
+		{
+			get => _shootDirection;
+			set => _shootDirection = value;
+		}
 
-        public int Ammo => _ammo;
+		public int Ammo => _ammo;
 
-        public bool IsDigging => _isDigging;
+		public bool IsDigging => _isDigging;
 
-        public float SpeedModifier { get; set; } = 1f;
+		public float SpeedModifier { get; set; } = 1f;
 
-        private bool CanShoot => _shootTimer <= 0f;
+		private bool CanShoot => _shootTimer <= 0f;
 
-        private void Awake()
-        {
-            _mainCam = Camera.main;
-            _rigidbody = GetComponent<Rigidbody2D>();
-        }
+		private void Awake()
+		{
+			_mainCam = Camera.main;
+			_rigidbody = GetComponent<Rigidbody2D>();
+		}
 
-        private void Update()
-        {
-            _shootTimer -= Time.deltaTime;
-        }
+		private void Start()
+		{
+			SpeedModifier = 1f;
+			_isDigging = false;
+			_slider.gameObject.SetActive(false);
+			_pickupAmmoObject.SetActive(false);
+		}
 
-        private void OnMove(InputValue value)
-        {
-            Input = value.Get<Vector2>();
-        }
+		private void Update()
+		{
+			_shootTimer -= Time.deltaTime;
+		}
 
-        private void OnShoot(InputValue value)
-        {
-            _mousePressed = value.Get<float>() > 0f;
-            if (_mousePressed)
-            {
-                Vector2 playerToMouse = GetPlayerToMouseVec();
-                _shootDirection = playerToMouse.normalized;
-            }
-            else
-            {
-                _shootDirection = Vector2.zero;
-            }
-        }
+		private void OnMove(InputValue value)
+		{
+			Input = value.Get<Vector2>();
+		}
 
-        private void OnLook(InputValue value)
-        {
-            _shootDirection = value.Get<Vector2>().normalized;
-        }
+		private void OnShoot(InputValue value)
+		{
+			_mousePressed = value.Get<float>() > 0f;
+			if (_mousePressed)
+			{
+				Vector2 playerToMouse = GetPlayerToMouseVec();
+				_shootDirection = playerToMouse.normalized;
+			}
+			else
+			{
+				_shootDirection = Vector2.zero;
+			}
+		}
 
-        private void OnDig(InputValue value)
-        {
-            bool pressed = value.Get<float>() > 0f;
-            if (pressed)
-            {
-                SpeedModifier = 0f;
-                _isDigging = true;
-                _slider.gameObject.SetActive(true);
-                _pickupAmmoObject.SetActive(true);
-            }
-            else
-            {
-                SpeedModifier = 1f;
-                _isDigging = false;
-                _slider.gameObject.SetActive(false);
-                _pickupAmmoObject.SetActive(false);
-            }
-        }
+		private void OnLook(InputValue value)
+		{
+			_shootDirection = value.Get<Vector2>().normalized;
+		}
 
-        private void FixedUpdate()
-        {
-            ApplyMovements();
-            HandleShoot();
-        }
+		private void OnDig(InputValue value)
+		{
+			bool pressed = value.Get<float>() > 0f;
+			if (pressed)
+			{
+				SpeedModifier = 0f;
+				_isDigging = true;
+				_slider.gameObject.SetActive(true);
+				_pickupAmmoObject.SetActive(true);
+			}
+			else
+			{
+				SpeedModifier = 1f;
+				_isDigging = false;
+				_slider.gameObject.SetActive(false);
+				_pickupAmmoObject.SetActive(false);
+			}
+		}
 
-        private void HandleShoot()
-        {
-            if (!CanShoot || _shootDirection == Vector2.zero) return;
+		private void FixedUpdate()
+		{
+			ApplyMovements();
+			HandleShoot();
+		}
 
-            Shoot(_mousePressed ? GetPlayerToMouseVec().normalized : _shootDirection);
-        }
+		private void HandleShoot()
+		{
+			if (!CanShoot || _shootDirection == Vector2.zero) return;
 
-        private void Shoot(Vector2 direction)
-        {
-            if (_ammo <= 0) return;
+			Shoot(_mousePressed ? GetPlayerToMouseVec().normalized : _shootDirection);
+		}
 
-            if (!_mousePressed)
-                _shootDirection = Vector2.zero;
+		private void Shoot(Vector2 direction)
+		{
+			if (_ammo <= 0) return;
 
-            _shootTimer = _timeToShotAgainInSeconds;
-            GameObject projectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
-            var behaviour = projectile.GetComponent<ProjectileBehaviour>();
-            behaviour.Shoot(direction);
-            _ammo -= 1;
-            _audioPlayer.Play();
-        }
+			if (!_mousePressed)
+				_shootDirection = Vector2.zero;
 
-        private void ApplyMovements()
-        {
-            var movement = new Vector2(_moveDirection.x, 0);
-            movement *= _moveSpeed * SpeedModifier;
-            movement.x += KnockbackVelocityX;
-            Movement = movement;
+			_shootTimer = _timeToShotAgainInSeconds;
+			GameObject projectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
+			var behaviour = projectile.GetComponent<ProjectileBehaviour>();
+			behaviour.Shoot(direction);
+			_ammo -= 1;
+			_audioPlayer.Play();
+		}
 
-            if (_maxLeft && _maxRight)
-            {
-                float posX = transform.position.x;
-                bool isOutOfBound = posX < _maxLeft.position.x || posX > _maxRight.position.x;
-                bool isGoingOutwards = Mathf.Sign(posX) * Mathf.Sign(Movement.x) > 0f;
+		private void ApplyMovements()
+		{
+			var movement = new Vector2(_moveDirection.x, 0);
+			movement *= _moveSpeed * SpeedModifier;
+			movement.x += KnockbackVelocityX;
+			Movement = movement;
 
-                if (isOutOfBound && isGoingOutwards)
-                {
-                    Movement = Vector2.zero;
-                }
-            }
+			if (_maxLeft && _maxRight)
+			{
+				float posX = transform.position.x;
+				bool isOutOfBound = posX < _maxLeft.position.x || posX > _maxRight.position.x;
+				bool isGoingOutwards = Mathf.Sign(posX) * Mathf.Sign(Movement.x) > 0f;
 
-            _rigidbody.velocity = Movement;
+				if (isOutOfBound && isGoingOutwards)
+				{
+					Movement = Vector2.zero;
+				}
+			}
 
-            if (!Mathf.Approximately(KnockbackVelocityX, 0f))
-            {
-                KnockbackVelocityX -= _knockbackDrag * Mathf.Sign(KnockbackVelocityX);
-            }
-        }
+			_rigidbody.velocity = Movement;
 
-        private Vector2 GetPlayerToMouseVec()
-        {
-            Vector2 pos = _mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            return pos - (Vector2) transform.position;
-        }
+			if (!Mathf.Approximately(KnockbackVelocityX, 0f))
+			{
+				KnockbackVelocityX -= _knockbackDrag * Mathf.Sign(KnockbackVelocityX);
+			}
+		}
+
+		private Vector2 GetPlayerToMouseVec()
+		{
+			Vector2 pos = _mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+			return pos - (Vector2) transform.position;
+		}
 
 
-        public void AddAmmo(int amount)
-        {
-            _ammo += amount;
-        }
-    }
+		public void AddAmmo(int amount)
+		{
+			_ammo += amount;
+		}
+	}
 }
