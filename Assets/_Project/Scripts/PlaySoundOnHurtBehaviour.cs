fa8947c4f10e794f912a6cc662e7560e1369efe2
Stowy <stowy@protonmail.ch>
2023-02-04 10:28:50 +0100

    added sound on hurt

---
+++ Assets/_Project/Scripts/PlaySoundOnHurtBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace GlobalGameJam
+{
+	[RequireComponent(typeof(AudioSource), typeof(Health))]
+	public class PlaySoundOnHurtBehaviour : MonoBehaviour
+	{
+		private AudioSource _audioSource;
+		private Health _health;
+
+		private void Awake()
+		{
+			_audioSource = GetComponent<AudioSource>();
+			_health = GetComponent<Health>();
+		}
+
+		private void OnEnable()
+		{
+			_health.OnHurt += OnHurt;
+		}
+
+		private void OnDisable()
+		{
+			_health.OnHurt -= OnHurt;
+		}
+
+		private void OnHurt(Transform _)
+		{
+			_audioSource.Play();
+		}
+	}
+}
