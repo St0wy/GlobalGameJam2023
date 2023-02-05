200bc5bc7a5a814c94ceb21ca03395ce25b3ad18
Stowy <stowy@protonmail.ch>
2023-02-05 01:52:27 +0100

    added teenager lvl up

---
+++ Assets/_Project/Scripts/PlayerUpgradeBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace GlobalGameJam
+{
+	[RequireComponent(typeof(Animator), typeof(Score))]
+	public class PlayerUpgradeBehaviour : MonoBehaviour
+	{
+		public enum UpgradeState
+		{
+			Flower = 0,
+			Teenager = 1,
+			Adult = 2,
+		}
+
+		[SerializeField]
+		private int _scoreUpgradeOne = 3000;
+
+		[SerializeField]
+		private int _scoreUpgradeTwo = 6000;
+
+		private Score _score;
+		private Animator _animator;
+		private static readonly int PlayerUpgradeState = Animator.StringToHash("PlayerUpgradeState");
+
+		public UpgradeState State { get; private set; }
+
+		private void Awake()
+		{
+			_animator = GetComponent<Animator>();
+			_score = GetComponent<Score>();
+		}
+
+		private void Update()
+		{
+			if (_score.PlayerScore >= _scoreUpgradeOne && _score.PlayerScore < _scoreUpgradeTwo)
+			{
+				State = UpgradeState.Teenager;
+			}
+			else if (_score.PlayerScore >= _scoreUpgradeTwo)
+			{
+				State = UpgradeState.Adult;
+			}
+
+			_animator.SetInteger(PlayerUpgradeState, (int) State);
+		}
+	}
+}
